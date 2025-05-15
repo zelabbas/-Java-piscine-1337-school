1af4891de9a1798db19329df77adeaba537a36c9
zelabbas <zakariaelabbassi2002@gmail.com>
2025-05-15 18:54:53 +0100

    remove files not needed

---
+++ src/main/java/com/zelabbas/Module00/ex04/Program.java
@@ -0,0 +1,9 @@
+package com.zelabbas.Module00.ex04;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello sir");
+    }
+}
--- src/main/java/com/zelabbas/test.java
+++
@@ -1,7 +0,0 @@
-package com.zelabbas;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
